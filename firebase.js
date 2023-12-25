39eb3423058d8fc33a5633f5c80055c722a436d4
davidvilmana <102814259+davidvilmana@users.noreply.github.com>
2023-12-25 16:56:20 -0500

    firebase.js

--- firebase.js
+++ firebase.js
@@ -11,5 +11,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 
-// Initialize Cloud Firestore and get a reference to the service
+// Initialize Cloud Fit a reference to the service
 const db = getFirestore(app);
